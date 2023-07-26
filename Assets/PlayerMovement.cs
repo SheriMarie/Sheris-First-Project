92b4dddfcbad7695df163610a6badd877bb328dd
SheriMarie <140038384+SheriMarie@users.noreply.github.com>
2023-07-26 14:08:10 +1000

    Added Delta Time

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -14,28 +14,27 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Rigidbody2D playerRB = GetComponent<Rigidbody2D>();
-            playerRB.AddForce(Vector2.up * 200f);
+            playerRB.AddForce(Vector2.up * 150f);
         }
 
         //Left movement 
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             Rigidbody2D playerRB = GetComponent<Rigidbody2D>();
-            playerRB.AddForce(Vector2.left * 200f);
+            playerRB.AddForce(Vector2.right * 600f * Time.deltaTime);
 
         }
 
         //Right movement 
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             Rigidbody2D playerRB = GetComponent<Rigidbody2D>();
-            playerRB.AddForce(Vector2.right * 200f);
+            playerRB.AddForce(Vector2.left * 600f * Time.deltaTime);
 
         }
     }
 
 }
 
-// Now work out how to use delta time when moving left and right  //Time.deltaTime; 
